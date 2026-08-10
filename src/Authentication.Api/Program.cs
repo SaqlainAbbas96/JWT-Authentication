3b2f32ad25aee76cafcdc0844f8188d3964860b0
SaqlainAbbas96 <saqlain572@gmail.com>
2026-08-10 17:42:49 +0500

    feat: implement validation and consistent ProblemDetails errors

--- src/Authentication.Api/Program.cs
+++ src/Authentication.Api/Program.cs
@@ -1,14 +1,17 @@
 using Authentication.Api.Endpoints;
+using Authentication.Api.ExceptionHandling;
 using Authentication.Api.Extensions;
 using Authentication.Application.Configuration;
 using Authentication.Application.Interfaces;
 using Authentication.Application.Services;
+using Authentication.Application.Validators;
 using Authentication.Infrastructure.Identity;
 using Authentication.Infrastructure.Persistence;
 using Authentication.Infrastructure.Repositories;
 using Authentication.Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +28,11 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<DBContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
 
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -45,6 +53,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 app.UseAuthentication();
 app.UseAuthorization();
---
+++ src/Authentication.Application/Validators/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Authentication.Application.Dtos.Requests;
+using FluentValidation;
+
+namespace Authentication.Application.Validators
+{
+    public sealed class LoginRequestValidator : AbstractValidator<LoginRequestDto>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
